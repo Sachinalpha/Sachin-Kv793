9e324c301c3873652e1eb28d64c6dc78dad23032
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-25 10:35:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,14 @@
-provider "azurerm" {
-  features {}
-}
-
-variable "resource_group_name" {}
-variable "location" {}
-variable "key_vault_name" {}
-
-# Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# Create Key Vault
 resource "azurerm_key_vault" "kv" {
   name                        = var.key_vault_name
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_retention_days  = 7
+  tenant_id                   = var.tenant_id
+  soft_delete_enabled         = true
   purge_protection_enabled    = false
 }
-
-data "azurerm_client_config" "current" {}

8f47c577835491d308097e477160febca6b93c85
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-25 10:49:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,24 +12,3 @@ variable "key_vault_name" {
   description = "Name of the Key Vault"
   type        = string
 }
-
-variable "client_id" {
-  description = "Service Principal Client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service Principal Client Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
